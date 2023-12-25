fda69ba0754b26ee1eb914ea09dcd4f94adc175b
Nistel <duz3000@gmail.com>
2023-12-25 12:21:41 +0300

    test : add tests in BunTest

--- src/test/java/praktikum/BunTest.java
+++ src/test/java/praktikum/BunTest.java
@@ -1,24 +1,31 @@
 package praktikum;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class BunTest {
 
+    private Bun bun;
+    private final String ANY_NAME_BUN = "anyName";
+    private final float ANY_PRICE_BUN = 100;
+    private final float CALCULATION_DEVIATION = 0.01F;
+
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
+        bun = new Bun(ANY_NAME_BUN, ANY_PRICE_BUN);
     }
 
     @Test
-    public void getName() {
+    public void testGetNameMethodNameCorrect() {
+        String nameInBun = bun.getName();
+        assertEquals("Ошибка в названия булки", ANY_NAME_BUN, nameInBun);
     }
 
     @Test
-    public void getPrice() {
-    }
-
-    @After
-    public void tearDown() {
+    public void testGetPriceMethodPriceCorrect() {
+        float priseInBun = bun.getPrice();
+        assertEquals("Ошибка в цене булки", ANY_PRICE_BUN, priseInBun, CALCULATION_DEVIATION);
     }
 }
