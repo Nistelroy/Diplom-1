462c4cd6b1b71c27e100fb43406efcb2ecb45de4
Nistel <duz3000@gmail.com>
2023-12-25 11:27:57 +0300

    test : add test classes

---
+++ src/test/java/praktikum/IngredientTest.java
@@ -0,0 +1,30 @@
+package praktikum;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class IngredientTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getPrice() {
+    }
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void getType() {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
---
+++ src/test/java/praktikum/IngredientTypeTest.java
@@ -0,0 +1,27 @@
+package praktikum;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class IngredientTypeTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void values() {
+    }
+
+    @Test
+    public void valueOf() {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+}
